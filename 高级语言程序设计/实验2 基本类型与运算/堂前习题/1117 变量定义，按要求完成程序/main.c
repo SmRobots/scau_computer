4124e7f4c27b9d92fd78276f0edd0001513c7a12
SmRobots <51939688+SmRobots@users.noreply.github.com>
2021-01-04 23:42:02 +0800

    Create main.c
    虽然它这里浮点数打印出来的是%.2f，但是试了一下使用double类型也是能通过测试的

---
+++ 高级语言程序设计/实验2 基本类型与运算/堂前习题/1117 变量定义，按要求完成程序/main.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+main()
+{
+    int a, b; /*定义整型变量a和b*/
+    float i, j; /*定义浮点变量i和j*/
+    a=5;
+    b=6;
+    i=3.14; j=i*a*b;
+    printf("a=%d,b=%d,i=%.2f,j=%.2f\n", a, b, i, j);
+}

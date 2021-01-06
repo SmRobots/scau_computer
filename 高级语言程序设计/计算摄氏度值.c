20273aaea9ad1cd95d09bad0b37a0472110e95a1
SmRobots <51939688+SmRobots@users.noreply.github.com>
2021-01-06 23:32:35 +0800

    Create 计算摄氏度值.c

---
+++ 高级语言程序设计/计算摄氏度值.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+	
+	
+	double a;
+	scanf("%lf", &a);
+	double b = 5.0 / 9 * (a - 32); 
+	printf("%.2lf", b);
+		
+	return 0;
+}
+
+
+

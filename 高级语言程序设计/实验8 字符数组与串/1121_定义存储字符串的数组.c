c529cec19d6025c8a0a7bbf580ac8f99487beb42
SmRobots <51939688+SmRobots@users.noreply.github.com>
2021-01-08 12:40:25 +0800

    Create 1121_定义存储字符串的数组.c
    这里长度题目也没说明啥

---
+++ 高级语言程序设计/实验8 字符数组与串/1121_定义存储字符串的数组.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main() {
+
+	char s[81];/*define a array named s to store string*/
+    strcpy(s, "abcdefghijklmn");
+    printf("%s", s);
+	
+	return 0;
+}
+
+
+

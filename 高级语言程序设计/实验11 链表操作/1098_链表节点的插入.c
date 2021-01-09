62dec15d8622523be001449a4071ff19d1d302bd
SmRobots <51939688+SmRobots@users.noreply.github.com>
2021-01-09 13:11:08 +0800

    Create 1098_链表节点的插入.c

---
+++ 高级语言程序设计/实验11 链表操作/1098_链表节点的插入.c
@@ -0,0 +1,74 @@
+  #include <stdio.h>
+#define LEN sizeof(struct student)
+
+struct student {
+	long num;
+	int score;
+	struct student *next;
+};
+
+struct student *create(int n) {
+	struct student *head=NULL,*p1=NULL,*p2=NULL;
+	int i;
+	for(i=1; i<=n; i++) {
+		p1=(struct student *)malloc(LEN);
+		scanf("%ld",&p1->num);
+		scanf("%d",&p1->score);
+		p1->next=NULL;
+		if(i==1) head=p1;
+		else p2->next=p1;
+		p2=p1;
+	}
+	return(head);
+}
+
+struct student *insert(struct student *head, struct student *stud) {
+	struct student *q, *p;
+	if (head == NULL) {
+		return stud;
+	}
+	q = head;
+	while (q->num < stud->num && q->next != NULL) {
+		p = q;
+		q = q->next;
+	}
+	if (q == NULL) {
+		q->next = stud;
+	} else {
+		stud->next = q;
+		p->next = stud;
+	}
+	return head;
+	
+}
+
+
+void print(struct student *head) {
+	struct student *p;
+	p=head;
+	while(p!=NULL) {
+		printf("%8ld%8d",p->num,p->score);
+		p=p->next;
+		printf("\n");
+	}
+}
+
+int main() {
+
+	struct student *head,*stu;
+    int n;
+    scanf("%d",&n);   
+    head=create(n);
+    print(head);
+    stu=(struct student *)malloc(LEN);
+    scanf("%ld",&stu->num);        
+    scanf("%d",&stu->score);    
+    stu->next = NULL;
+    head=insert(head,stu);
+    print(head);
+
+	return 0;
+}
+
+
+
